5e7d1ec733b5087d32703f7e15b8eb6b51ba6baa
minseokKim6823 <alstjr6823@naver.com>
2024-08-19 18:17:59 +0900

    spring&mybatis log4j2를 이용한 로그 확인

---
+++ chap01_springmybatis-lecture-source/src/test/java/com/ohgiraffers/springmybatis/Chap01SpringmybatisLectureSourceApplicationTests.java
@@ -0,0 +1,14 @@
+
+package com.ohgiraffers.springmybatis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Chap01SpringmybatisLectureSourceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
